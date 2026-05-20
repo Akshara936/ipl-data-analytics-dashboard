cefee1795ecba0b3c6616a4eacfe70756c1809c6
Akshara936 <aksharasambu06@gmail.com>
2026-05-20 22:41:06 +0530

    Delete death_overs.sql

--- death_overs.sql
+++
@@ -1,9 +0,0 @@
-
--- Purpose: Analyze runs scored in death overs
-
-SELECT batting_team,
-SUM(total_runs) AS death_over_runs
-FROM deliveries
-WHERE `over` BETWEEN 16 AND 20
-GROUP BY batting_team
-ORDER BY death_over_runs DESC;
