33e4359aa909c6027b8800258925d5650b4acb54
Akshara936 <aksharasambu06@gmail.com>
2026-05-20 22:42:04 +0530

    Delete top_batters.sql

--- top_batters.sql
+++
@@ -1,8 +0,0 @@
--- Purpose: Find top 10 run scorers in IPL
-USE ipl_analysis;
-SELECT batsman,
-SUM(batsman_runs) AS total_runs
-FROM deliveries
-GROUP BY batsman
-ORDER BY total_runs DESC
-LIMIT 10;
