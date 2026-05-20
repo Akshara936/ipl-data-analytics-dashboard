f4bbbee0f1af97715503339ccf1de2706a801022
Akshara936 <aksharasambu06@gmail.com>
2026-05-20 22:42:44 +0530

    Delete top_bowlers.sql

--- top_bowlers.sql
+++
@@ -1,9 +0,0 @@
--- Purpose: Find bowlers with most wickets
-
-SELECT bowler,
-COUNT(*) AS wickets
-FROM deliveries
-WHERE player_dismissed IS NOT NULL
-GROUP BY bowler
-ORDER BY wickets DESC
-LIMIT 10;
